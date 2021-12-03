4f4b0d2998184e4aa1fcff0e83fd959f28417290
Tuğkan <tugkanayhan@gmail.com>
2021-12-03 16:28:37 +0300

    Files added

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,7 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:qr_code/pages/qr/qr_generate.dart';
 import 'package:qr_code/pages/qr/qr_scan.dart';
-import 'package:intl_phone_number_input/intl_phone_number_input.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -11,6 +12,8 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  TextEditingController phoneController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,7 +44,8 @@ class _HomeState extends State<Home> {
             /* Second Button */
             ElevatedButton(
               onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) => const QrGenerate()));
+                String phone = phoneController.text;
+                _findUser(phone);
               },
               child: const Text("Generate QR Code"),
             ),
@@ -53,8 +57,34 @@ class _HomeState extends State<Home> {
   }
 
   Widget _getPhoneNumber() {
-    return InternationalPhoneNumberInput(
-      onInputChanged: (value) {},
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: TextField(
+        controller: phoneController,
+        keyboardType: TextInputType.phone,
+        maxLength: 10,
+        textAlign: TextAlign.center,
+        decoration: const InputDecoration(
+          hintText: "Müşteri Telefon Numarası",
+          border: OutlineInputBorder(),
+        ),
+      ),
     );
+  }
+
+  Future _findUser(String phone) async {
+    // Customer as QuerySnapshot
+    QuerySnapshot customer = await FirebaseFirestore.instance.collection('customers').where('phone', isEqualTo: phone).get();
+    // Getting document snapshot of the query
+    QueryDocumentSnapshot doc = customer.docs[0];
+    // Document reference of that snapshot
+    DocumentReference docRef = doc.reference;
+
+    Fluttertoast.showToast(
+      msg: "Phone: $phone, UserID: ${docRef.id}",
+      toastLength: Toast.LENGTH_LONG,
+    );
+
+    Navigator.push(context, MaterialPageRoute(builder: (context) => QrGenerate(phoneNumber: phone)));
   }
 }
--- lib/pages/qr/qr_generate.dart
+++ lib/pages/qr/qr_generate.dart
@@ -1,23 +1,7 @@
 import 'package:flutter/material.dart';
 
 class QrGenerate extends StatelessWidget {
-  const QrGenerate({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text("CarpeTiem - QR Kod Oluşturma"),
-        backgroundColor: Colors.grey[900],
-      ),
-      backgroundColor: Colors.blueGrey[200],
-    );
-  }
-}
-
-class PhoneNumber extends StatelessWidget {
-  const PhoneNumber({Key? key}) : super(key: key);
+  const QrGenerate({Key? key, required String phoneNumber}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
